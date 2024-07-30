fc960de1aa6feb9c6fdf60e0d4bc2efec0a421f8
unknown <gauravkshatriya99@gmail.com>
2024-07-30 15:01:28 +0530

    config fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/vanLife-master'
+  base: '/{vanLife-master}/'  // Update this to match your repository name
 })
